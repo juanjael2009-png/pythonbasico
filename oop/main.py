c246b02beb892116cd6ca04049f639917f8d50c7
juanjael2009-png <juanjael2009@gmail.com>
2026-03-02 16:07:53 -0800

    main

--- oop/main.py
+++ oop/main.py
@@ -7,3 +7,39 @@ ogro1 = Ogro(20, 3)
 
 print(f"{zombie1.get_tipo_enemigo()} tiene {zombie1.puntos_energia} de energia y ataca con {zombie1.ataque}")
 print(f"{ogro1.get_tipo_enemigo()} tiene {ogro1.puntos_energia} de energia y ataca con {ogro1.ataque}")
+
+def batalla(e1:Enemigo,e2: Enemigo):
+    e1.habla() 
+    e2.habla()
+    while e1.puntos_energia > 0 and e2.puntos_energia > 0:
+        print("##############")
+        e2.ataque_especial()
+        e1.ataque_especial()
+
+        print(f"{e1.get_tipo_enemigo()} ahora: {e1.puntos_energia} puntos de energía!!!")
+        print(f"{e2.get_tipo_enemigo()} ahora: {e2.puntos_energia} puntos de energía!!!")
+        print(f"Ataque: {e2.ataque}")
+        e1.puntos_energia -= e2.ataque
+        print("===========")
+
+        print(f"Ataque: {e1.ataque}")
+        e2.puntos_energia -= e1.ataque
+        print("******")
+       
+        if e1.puntos_energia > 0:
+         print(f"{e1.get_tipo_enemigo()} ganó!!!")
+
+        else:
+         print(f"{e2.get_tipo_enemigo()} ganó!!!")
+               
+               
+        print("-----------BATALLA------------")
+    batalla(Zombie, Ogro)
+    print("-------FIN DE LA BATALLA-------------")
+    #print(f"{zombie.get_tipo_enemigo()} tiene {zombie.puntos_energia} de energia y ataca con {zombie.ataque}")
+    #print(f"{ogro.get_tipo_enemigo()} tiene {ogro.puntos_energia} de energia y ataca con {ogro.ataque}")
+
+
+
+
+
--- oop/ogro.py
+++ oop/ogro.py
@@ -1,9 +1,16 @@
 from enemigo import *
-
+import random
 class Ogro(Enemigo):
     def ___init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
         super().___init__(tipo_enemigo = "Ogro", puntos_energia=puntos_energia, ataque=ataque)
 
 
     def habla(self):
-        print("Ogro aplastar todo!!!")
+        print("Ogro aplastar todo!!!") 
+
+        def ataque_especial(self):
+            print("ogro ataque especial")
+            funcion_ataque_especial = random.random() < 0.20
+            if funcion_ataque_especial:
+                self.ataque += 4
+                print("ogro enojado y incremento su ataque por 4")
